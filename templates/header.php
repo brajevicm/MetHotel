7db7443b3fd540aa988d645eb99dcfab55dc98b8
Milos Brajevic <milos.brajevic@hotmail.com>
2017-03-06 10:49:38 +0100

    Added header and footer.

---
+++ templates/header.php
@@ -0,0 +1,55 @@
+<div id="header-wrapper">
+            <div id="logo">
+                <a href="index.html">
+                    <img src="images/logo2.png" alt="Hotel Metropolitan">
+                </a>
+            </div>
+            <div id="header-top">
+                <div class="social-top">
+                    <a href="#">
+                        <div class="social-icon">
+                            <img src="images/facebook.png" alt="">
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="social-icon">
+                            <img src="images/twitter.png" alt="">
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="social-icon">
+                            <img src="images/linkedin.png" alt="">
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div id="header-bot">
+                <div id="nav">
+                    <a href="#">
+                        <div class="menuitem">
+                            O hotelu
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="menuitem">
+                            Kongresni centar
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="menuitem">
+                            Wellness centar
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="menuitem">
+                            Cenovnik
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="menuitem">
+                            Galerija
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
