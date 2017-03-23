f43aa1b7173835210635f37dafbdd658896e4bce
Milos Brajevic <milos.brajevic@hotmail.com>
2017-03-23 01:39:27 +0100

    Added basic JS and LESS.

---
+++ js/slideshow.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var slides = window.document.querySelectorAll("#slides .slide");
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide, 4000);
+
+    function nextSlide() {
+        slides[currentSlide].className = "slide";
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].className = "slide active";
+    };
+});
